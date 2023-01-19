f20858143c769c2e2b4c6ecfa31813c10f873ddb
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2023-01-19 14:56:58 +0100

    update app js text

--- landing/src/App.js
+++ landing/src/App.js
@@ -9,8 +9,9 @@ function App() {
   <div>
   <Cards></Cards>
   </div>
-  <h2 className="font-bold text-center pt-12 text-5xl  text-blue-700 pt-1">GMT CHANGE</h2>
-  <hr class="border-4 m-auto border-yellow-500 w-1/12  drop-shadow-xl"/>
+  <h2 className="font-bold text-center pt-12 md:text-5xl text-xl pt-36 text-blue-700 pt-1">GMT CHANGE</h2>
+  <hr class="border-2 mt-6 m-auto border-yellow-500 w-1/12  drop-shadow-xl"/>
+  <h3 className="px-3 py-2 m-auto font-light text-center text-2xl items-center leading-snug text-neutral-500 ">Change - Métaux précieux - Transfert de fonds</h3>
 </div>
   );
 }
