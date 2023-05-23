3b50db33403fce5f644ddf13738912250f23fb01
Nicolas Cozzarin <11572984+ncozzarin@users.noreply.github.com>
2023-05-23 16:27:24 +0200

    updating the API from Bardh

--- src/components/changeCard/ChangeCard.jsx
+++ src/components/changeCard/ChangeCard.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import CurrencySelector from "../currencySelector/CurrencySelector";
 import MoneyInput from "../MoneyImput/MoneyImput";
+import axios from 'axios';
 
 const currency  = [
   {
@@ -43,6 +44,7 @@ const currency  = [
 
 export default function ChangeCardEn() {
 
+  const getRates = () => {}
   const [currencyOption1, setCurrencyOption1] = useState(currency[1]);
   const [currencyOption2, setCurrencyOption2] = useState(currency[2]);
   const [value1, setValue1] = useState(1);
@@ -54,39 +56,39 @@ export default function ChangeCardEn() {
 
   const [rates, setRates] = useState(null);
 
+  const fetchRates = async (fromCurrency, toCurrency, amount) => {
+    try {
+      const response = await axios.get('https://exchange.ufc.ch/api/rates', {
+        params: {
+          amount: amount,
+          from: fromCurrency,
+          to: toCurrency
+        },
+        mode: 'no-cors'
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    fetch('https://exchange.ufc.ch/api/rates?amount=100&from=CHF&to=USD', {
-    })
-      .then(res => console.log(res))
+    fetchRates('CHF','EUR',1);
   }, []);
 
   useEffect(()=>{
     setOptions2(currency.filter(o => o.id !== currencyOption1.id));
     setOptions1(currency.filter(o => o.id !== currencyOption2.id));
     if(rates){
-    changeCalculation1(value1);
+    changeCalculation1(value1); // we need to call this function properly, without the if
   }
   }, [rates, currencyOption1, currencyOption2]);
 
 
-  const changeCalculation1 = value => {
-    if(currencyOption1.name === 'CHF'){
-      const index = rates.findIndex(obj => obj.curr_code === currencyOption2.name);
-      if(value >= 1000 && currencyOption2.name === 'EUR'){
-        setValue2(value/rates[index].taux_VC)
-      } else {
-        setValue2(value/rates[index].taux_VA)
-      }
-    } else {
-      if(currencyOption2.name === 'CHF'){
-        const index = rates.findIndex(obj => obj.curr_code === currencyOption1.name);
-        setValue2(rates[index].taux_AA*value)
-      } else {
-      const INDEXa = rates.findIndex(obj => obj.curr_code === currencyOption2.name);
-      const indexV = rates.findIndex(obj => obj.curr_code === currencyOption1.name);
-      setValue2((rates[INDEXa].taux_AC*value)/rates[indexV].taux_VA)
-    }
-    }
+  const changeCalculation1 =  async (value) => {
+    const amount = await fetchRates(currencyOption1.name,currencyOption2.name,value);
+    setValue2(amount.data.converted_amount);
+      
   }
 
   const changeCalculation2 = value => {
