cd1f57e019fcc8e10e4dfa5d53b2a35d6311fcb9
Nicolas Cozzarin <nicolasjcozzarin@gmail.com>
2023-05-31 11:31:30 +0200

    copy updates for echange rates

--- src/components/currencyConverter/CurrencyConverter.jsx
+++ src/components/currencyConverter/CurrencyConverter.jsx
@@ -10,9 +10,9 @@ export default function CurrencyConverter() {
     <div className=" w-full rounded-b-xl">
         <h2 className="font-bold text-center lg:pt-12 pt-8 lg:text-4xl text-xl text-blue-700 ">TAUX DE CHANGE DU JOUR</h2>
         <hr className="border-2 mt-6 m-auto border-yellow-500 w-1/12  drop-shadow-xl"/>
-        <h3 className="lg:px-12 px-2 lg:pt-12 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">GMT Change, leader sur le marché du change à 
-Genève, avec certainement un des meilleurs taux de change à la vente et à l’achat, Ce taux est susceptible d’être modifié à tout moment.</h3>
-        <p className="px-12 lg:py-4 py-2 m-auto font-light text-center lg:text-xl text-base items-center leading-snug text-neutral-500  ">Derniere mise a jour: {date} </p>
+        <h3 className="lg:px-12 px-2 lg:pt-12 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">Si vous cherchez le meilleur endroit pour échanger votre monnaie, vous êtes au bon endroit.</h3>
+        <h3 className="lg:px-12 px-2 lg:pt-2 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">Avec des taux de change compétitifs et une équipe de professionnels expérimentés à votre disposition, nous sommes là pour vous aider à obtenir le meilleur taux pour votre argent. Que vous soyez un voyageur en viste sur Genève, un homme d'affaires, une frontalier travaillant sur Genève ou simplement quelqu'un qui a besoin de changer de l'argent pour des raisons personnelles, nous sommes là pour vous aider. Convaincus que vous ne trouverez pas de meilleur endroit pour changer votre argent que chez nous,  nous vous invitons à utiliser nos services dès maintenant ou à nous contacer pour en savoir plus.</h3>
+        <h3 className="lg:px-12 px-2 lg:pt-2 pt-4 lg:pb-12 pb-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">"Fixez & Bloquez le Taux". Le taux étant susceptible de changer à tout moment, vous pouvez fixer le taux avec nous lorsqu'il vous semble intéréssant. Rendez-vous ensuite auprès d'une de nos agences pour faire le change. Votre taux préferentiel est préservé pendant 3 heures à partir de votre fixation. Derniere mise a jour: {date}</h3>
         <ChangeCard></ChangeCard>
     </div>
   );
--- src/components/currencyConverterEn/currencyConverterEn.jsx
+++ src/components/currencyConverterEn/currencyConverterEn.jsx
@@ -11,9 +11,10 @@ export default function CurrencyConverterEn() {
     <div className=" w-full rounded-b-xl">
         <h2 className="font-bold text-center lg:pt-12 pt-8 lg:text-4xl text-xl text-blue-700 ">TODAY’S EXCHANGE RATE</h2>
         <hr className="border-2 mt-6 m-auto border-yellow-500 w-1/12  drop-shadow-xl"/>
-        <h3 className="lg:px-12 px-2 lg:pt-12 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">GMT Change, leader in the foreign exchange market at
-Geneva, with certainly one of the best exchange rates for sale and purchase, This rate is subject to change at any time.</h3>
-        <p className="px-12 lg:py-4 py-2 m-auto font-light text-center lg:text-xl text-base items-center leading-snug text-neutral-500 ">Last update: {date} </p>
+        <h3 className="lg:px-12 px-2 lg:pt-12 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">If you are looking for the best place to exchange your currency, you have come to the right place.</h3>
+        <h3 className="lg:px-12 px-2 lg:pt-2 pt-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">With competitive exchange rates and a team of experienced professionals at your disposal, we are here to help you get the best rate for your money. Whether you are a traveler visiting Geneva, a businessman, a cross-border commuter working in Geneva or simply someone who needs to exchange money for personal reasons, we are here to help. Convinced that you will not find a better place to change your money than with us, we invite you to use our services now or to contact us for more information.
+</h3>
+        <h3 className="lg:px-12 px-2 lg:pt-2 pt-4 lg:pb-12 pb-4 m-auto font-light text-center lg:text-2xl text-base items-center leading-snug text-neutral-500 ">Set & Lock Rate. Since the rate is subject to change at any time, you can lock in the rate with us when it looks good to you. Then go to one of our branches to make the change. Your preferred rate is preserved for 3 hours from the time of fixing. Last update: {date}</h3>
         <ChangeCardEn></ChangeCardEn>
     </div>
   );
